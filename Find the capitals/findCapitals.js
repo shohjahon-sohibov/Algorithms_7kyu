aa35a83c2c0bc2ba7d775b11356bfd78d070bab4
shohjahon-sohibov <sohibov.s@bk.ru>
2023-04-28 23:44:50 +0300

    write task

---
+++ Find the capitals/findCapitals.js
@@ -0,0 +1,16 @@
+/*
+
+Title =========
+
+    Find the capitals
+
+Task ==========
+    Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
+
+Example
+    Test.assertSimilar( capitals('CodEWaRs'), [0,3,4,6] );
+*/
+
+// Answer
+
+
