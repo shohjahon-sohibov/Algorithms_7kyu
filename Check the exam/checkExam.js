366ef175dda9425948f784da3c978d9a128570f9
shohjahon-sohibov <sohibov.s@bk.ru>
2023-04-30 01:19:01 +0300

    code answer

--- Check the exam/checkExam.js
+++ Check the exam/checkExam.js
@@ -17,3 +17,17 @@ Example ===========
 
 // Answer
 
+function checkExam(array1, array2) {
+    let score = 0
+    for(let i = 0; i < array1.length; i++) {
+      console.log(array1[i], array2[i])
+      if(array1[i] === array2[i]) {
+        score += 4
+      } else if(array2[i] == "") {
+        score
+      } else if(array1[i] !== array2[i]) {
+        score--
+      }
+    }
+    return score < 0 ? 0 : score
+  }
