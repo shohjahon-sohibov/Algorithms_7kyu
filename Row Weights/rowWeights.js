0c8b69e2a283ac86771c112ecbf921b97e456729
shohjahon-sohibov <sohibov.s@bk.ru>
2023-05-05 23:34:11 +0300

    initial

---
+++ Row Weights/rowWeights.js
@@ -0,0 +1,17 @@
+/* 
+Title ==========
+    Row Weights
+
+Task ========
+    Given an array of positive integers (the weights of the people), return a new array/tuple of two integers, where the first one is the total weight of team 1, and the second one is the total weight of team 2.
+
+Example ========
+    rowWeights([13, 27, 49])  ==>  return (62, 27)
+
+Note ========
+    Array size is at least 1.
+    All numbers will be positive
+*/
+
+// Answer
+
