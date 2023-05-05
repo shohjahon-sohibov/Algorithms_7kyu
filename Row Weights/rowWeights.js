2d5b307b287ab383a66363f7222066cb646a040f
shohjahon-sohibov <sohibov.s@bk.ru>
2023-05-05 23:39:34 +0300

    coding answer

--- Row Weights/rowWeights.js
+++ Row Weights/rowWeights.js
@@ -15,3 +15,12 @@ Note ========
 
 // Answer
 
+function rowWeights(array){
+    let team1 = 0;
+    let team2 = 0;
+    
+    for (let i = 0; i < array.length; i++) {
+      i % 2 != 1 ? team1 += array[i] : team2 += array[i]
+    }
+    return [team1, team2]
+  }
