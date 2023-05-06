2fcee4a7615a1a7e7863d5aa11e328bc20a9ad3b
shohjahon-sohibov <sohibov.s@bk.ru>
2023-05-07 02:00:39 +0300

    initial

---
+++ Sorted yes no how/sortedYesNoHow.js
@@ -0,0 +1,18 @@
+/* 
+Title =====
+    Sorted? yes? no? how?
+
+Task ========
+    Complete the method which accepts an array of integers, and returns one of the following:
+
+    "yes, ascending" - if the numbers in the array are sorted in an ascending order
+    "yes, descending" - if the numbers in the array are sorted in a descending order
+    "no" - otherwise
+    
+Note ======
+    You can assume the array will always be valid, and there will always be one correct answer.
+
+*/
+
+// Answer
+
