50ecf5e59ddaa6b95c5a9cafeed7aabbc3998bb7
shohjahon-sohibov <sohibov.s@bk.ru>
2023-05-08 20:18:33 +0300

    initial

---
+++ No oddities here/No oddities here.js
@@ -0,0 +1,13 @@
+/* 
+Title =========
+    No oddities here
+
+Task =========
+    Write a small function that returns the values of an array that are not odd.
+
+    All values in the array will be integers. Return the good values in the order they are given.
+
+*/
+
+// Answer
+
