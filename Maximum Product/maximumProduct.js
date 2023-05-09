d4e4c51dbf581ade4b760f006d24b42984410a02
shohjahon-sohibov <sohibov.s@bk.ru>
2023-05-09 05:00:41 +0300

    initial and write task

---
+++ Maximum Product/maximumProduct.js
@@ -0,0 +1,19 @@
+/* 
+Title =========
+    Maximum Product
+
+Task =======
+    Given an array of integers , Find the maximum product obtained from multiplying 2 adjacent numbers in the array.
+
+Note ============
+    Array/list size is at least 2.
+
+    Array/list numbers could be a mixture of positives, negatives also zeroes .
+
+Example ========
+    adjacentElementsProduct([1, 2, 3]); ==> return 6
+
+*/
+
+// Answer
+
