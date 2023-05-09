7bc6b3c15aa7fbfad73e5bd2e9d38cfc81e0226d
shohjahon-sohibov <sohibov.s@bk.ru>
2023-05-09 04:12:58 +0300

    create file and write solution

---
+++ Fizz Buzz/fizzBuzz.js
@@ -0,0 +1,36 @@
+/* 
+Title ========
+    Fizz Buzz
+
+Task =========
+    Return an array containing the numbers from 1 to N, where N is the parametered value.
+
+    Replace certain values however if any of the following conditions are met:
+
+    If the value is a multiple of 3: use the value "Fizz" instead
+    If the value is a multiple of 5: use the value "Buzz" instead
+    If the value is a multiple of 3 & 5: use the value "FizzBuzz" instead
+    N will never be less than 1.
+
+Example ==========
+    fizzbuzz(3) -->  [1, 2, "Fizz"]
+
+*/
+
+// Answer
+
+function fizzbuzz(n) {
+  let resArr = [];
+  for (let i = 1; i <= n; i++) {
+   if(i % 3 === 0 && i % 5 === 0) {
+     resArr.push('FizzBuzz')
+   } else if(i % 3 === 0) {
+     resArr.push('Fizz')
+   } else if(i % 5 === 0) {
+     resArr.push('Buzz')
+   } else {
+     resArr.push(i)
+   }
+ }
+  return resArr
+}
